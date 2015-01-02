e768797f6a62471c6bc0e77ccb444d8f4a94f4b0
frenata <danweasel@gmail.com>
2015-01-02 23:29:17 +0700

    removed obsolete code

--- complexconv.go
+++ complexconv.go
@@ -3,12 +3,12 @@ package complexconv
 import (
 	"errors"
 	//"fmt"
-	//"regexp"
 	"strconv"
 	"strings"
 )
 
-//
+// TODO: write String() function, to convert complex numbers to strings
+
 // isImagString returns true if s is a properly formed represenation of an imaginary number
 func isImagStr(s string) bool {
 	return strings.Contains(s, "i") && s[len(s)-1] == 'i'
@@ -23,20 +23,6 @@ func imagStr(s string) string {
 // splitComplex takes a string represenation of a complex number, and
 // splits it correctly into real and imaginary parts.
 func splitComplex(s string) (rstr string, istr string, err error) {
-	/*eReg := regexp.MustCompile("[eE]\\+")    // for searching for an 'e'  next to a '+'
-	eSplit := regexp.MustCompile("[^eE]\\+") // for splitting without throwing away 'e'
-	split := regexp.MustCompile("\\+")       // for splitting on the +
-	var sarray []string
-	fmt.Println(s)
-
-	if eReg.FindString(s) != "" {
-		sarray = eSplit.Split(s, 2)
-	} else {
-		sarray = split.Split(s, 2)
-	}
-	fmt.Println(sarray)*/
-
-	// sarray := make([]string, 2)  // slice of strings to hold parsed substrings
 
 	s1, s2 := s, "" // substrings vars, unknown real/img order
 	for i := range s {
@@ -49,8 +35,6 @@ func splitComplex(s string) (rstr string, istr string, err error) {
 			}
 		}
 	}
-
-	//fmt.Println(s1, s2)
 
 	switch { // check substrings, identify imag/real parts, there can be only one imag
 	case isImagStr(s1) && !isImagStr(s2): // is the first string imag and the second real
@@ -68,35 +52,6 @@ func splitComplex(s string) (rstr string, istr string, err error) {
 		return "", "", err
 	}
 
-	/* if sarray[1] != "" {
-		if s := sarray[0]; !strings.Contains(s, "i") {
-			rstr = s
-		} else {
-			err = errors.New("malformed complex number")
-			return
-		}
-		if s := sarray[1]; strings.Contains(s, "i") {
-			istr = s
-		} else {
-			err = errors.New("malformed complex number")
-			return
-		}
-	} else if s := sarray[0]; strings.Contains(s, "i") {
-		istr = s
-	} else {
-		rstr = s
-	}
-
-	fmt.Println(rstr, istr)
-
-	if strings.Contains(istr, "i") {
-		if istr[len(istr)-1] == 'i' {
-			istr = istr[:len(istr)-1]
-		}
-	} else {
-		//err = errors.New("no imaginary component")
-	}*/
-
 	return rstr, istr, nil
 }
 
@@ -109,27 +64,19 @@ func ParseComplex(s string, bitSize int) (c complex128, err error) {
 		return c, err
 	}
 
-	//fmt.Printf("real part: %v, img part: %v\n", rstr, istr)
-
-	//if rstr != "" {
 	r, err = strconv.ParseFloat(rstr, bitSize)
 	if err != nil && rstr != "" { // ignore ParseFloat error from "", 0.0 value is correct
 		//fmt.Println(err)
 		//fmt.Println("real part error")
 		return c, err
 	}
-	//}
 
-	//if istr != "" {
 	i, err = strconv.ParseFloat(istr, bitSize)
 	if err != nil && istr != "" { // ignore error from "", 0.0 value is correct
 		//fmt.Println(err)
 		//fmt.Println("img part error")
 		return c, err
 	}
-	//}
-	//fmt.Println(r, i)
-	//c, err = complex(r, i), nil
 
 	return complex(r, i), nil
 }
--- complexconv_test.go
+++ complexconv_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 )
 
-var cmplx = []complex128{0i, 011i, 0.i, 2.71828, 1E6i, 1.e+0i, 0, 3.0 + 2i, 2.7e-4 + 2i, 7.7e+4 + 0.9i, 3i + 4.0, 2.1i + 4.55e+10}
-var str = []string{"0i", "011i", "0.i", "2.71828", "1E6i", "1.e+0i", "0", "3+2i", "2.7e-4+2i", "7.7e+4+0.9i", "3i+4.0", "2.1i+4.55e+10"}
+var cmplx = []complex128{0i, 011i, 0.i, 2.71828, 1E6i, 1.e+0i, 0, 3.0 + 2i, 2.7e-4 + 2i, 7.7e+4 + 0.9i, 3i + 4.0, 2.1i + 4.55e+10, 0}
+var str = []string{"0i", "011i", "0.i", "2.71828", "1E6i", "1.e+0i", "0", "3+2i", "2.7e-4+2i", "7.7e+4+0.9i", "3i+4.0", "2.1i+4.55e+10", ""}
 
 func Test_ParseComplex(t *testing.T) {
 	for i := range str {
@@ -15,3 +15,5 @@ func Test_ParseComplex(t *testing.T) {
 		}
 	}
 }
+
+// TODO: tests for complex to string, tests for expected errors
