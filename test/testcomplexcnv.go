469117d7999d9a3587d7cbb44dc7837a44b2a144
Andrew Nichols <danweasel@gmail.com>
2015-01-10 00:20:14 +0700

    test

--- test/testcomplexcnv.go
+++ test/testcomplexcnv.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-	. "github.com/frenata/complexconv"
+	cc "github.com/frenata/complexconv"
 	"reflect"
 	"strconv"
 )
 
 func main() {
 	s := "1e+6i"
-	c, e := ParseComplex(s)
+	c, e := cc.ParseComplex(s)
 	fmt.Println(c, e)
-	c, e = ParseComplex("999")
+	c, e = cc.ParseComplex("999")
 	fmt.Println(c, e)
-	c, e = ParseComplex("2.33e+4+2i")
+	c, e = cc.ParseComplex("2.33e+4+2i")
 	fmt.Println(c, e)
 	s = "2.4"
 	fmt.Println(strconv.ParseFloat(s, 64))
